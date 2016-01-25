5a32258d52b4eaa290c361c0df982069a7635fb7
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2016-01-25 23:26:25 +0100

    Removed double Pirates map

--- site/js/Customizations.js
+++ site/js/Customizations.js
@@ -92,15 +92,6 @@ baseLayers.push(myBackgroundLayer);
 });
 baseLayers.push(myBackgroundLayer);
 
-
-baseLayers.push(myBackgroundLayer);
-  var myBackgroundLayer = new OpenLayers.Layer.WMS("Historic map",
-    ["https://a.tiles.mapbox.com/v4/reinier.onp8ndmf/${z}/${x}/${y}.png?access_token=pk.eyJ1IjoicmVpbmllciIsImEiOiJYTHBYeW9jIn0.vhzQp9gepxVOTgRrjWMW8A"], {
-    sphericalMercator: true,
-    wrapDateLine: true,
-    isBaseLayer: true,
-});
-
 // Path to map
   var servername = location.href.split(/\/+/)[1];
   mappath = "http://"+servername;
